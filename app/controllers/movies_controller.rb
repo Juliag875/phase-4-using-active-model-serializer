6a4ebb765a82ec26b8127a3da6e4c090775f8624
Iuliia El Sammak <juliag875@gmail.com>
2022-03-19 18:14:36 -0400

    commit tree 0960ee04b770db187032468f53df92fb13923950

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -10,6 +10,16 @@ class MoviesController < ApplicationController
     movie = Movie.find(params[:id])
     render json: movie
   end
+#custom serializer solely for displaying our movie summary
+  def summary
+    movie = Movie.find(params[:id])
+    render json: movie, serializer: MovieSummarySerializer
+  end
+
+  #to render the full collection of movies
+  def summaries
+    movie = Movie.all
+    render json: movies, each_serializer: MovieSummarySerializer
 
   private
 
